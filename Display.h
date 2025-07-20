3c9d9914525fbc6bf0345dd4e3736aa413d84a5a
giovannilupi <giovanni.lupi1@edu.unifi.it>
2025-07-20 15:18:01 +0200

    Created Subject, Observer and Display classes

---
+++ Display.h
@@ -0,0 +1,15 @@
+//
+// Created by gionimbus on 7/20/25.
+//
+
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+class Display {
+public:
+    virtual ~Display() {}
+
+    virtual void draw() = 0;
+};
+
+#endif //DISPLAY_H
---
+++ Subject.h
@@ -0,0 +1,20 @@
+//
+// Created by gionimbus on 7/20/25.
+//
+
+#ifndef SUBJECT_H
+#define SUBJECT_H
+
+#include "Observer.h"
+
+class Subject {
+public:
+    virtual ~Subject() {}
+
+    virtual void notify() = 0;
+
+    virtual void subscribe(Observer* o) = 0;
+    virtual void unsubscribe(Observer* o) = 0;
+};
+
+#endif //SUBJECT_H
