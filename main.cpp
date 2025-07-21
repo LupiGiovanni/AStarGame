17f3a6e1c3a628e270c044f96fb6258af323e8fd
giovannilupi <giovanni.lupi1@edu.unifi.it>
2025-07-21 15:39:54 +0200

    Added helper function displayPath in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
+#include "GlobalMap.h"
+#include "GameCharacter.h"
+#include "SearchState.h"
+
+bool displayPath (const GlobalMap& map, int width, int height, const std::vector<SearchState>& path) {
+    if (path.empty())
+        return false;
+
+    SearchState start = path.front();
+    SearchState goal = path.back();
+
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            bool isPath = false;
+            for (auto it = path.begin(); it != path.end(); ++it) {
+                if (it->getX() == x && it->getY() == y) {
+                    if (it == path.begin()) {
+                        std::cout << "S  ";
+                    } else if (it == path.end() - 1) {
+                        std::cout << "G  ";
+                    } else {
+                        std::cout << "+  ";
+                    }
+                    isPath = true;
+                    break;
+                }
+            }
+            if (!isPath) {
+                if (map.getValue(x ,y) == 9)
+                    std::cout << "#  ";
+                else if (map.getValue(x, y) < 9)
+                    std::cout << ".  ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    return true;
+}
 
 int main() {
-    return 0;
+
 }
