a5c4a818ad613033d76eb037c06ec40ffa224fb5
giovannilupi <giovanni.lupi1@edu.unifi.it>
2025-07-23 00:21:55 +0200

    Created MapView class

---
+++ MapView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gionimbus on 7/23/25.
+//
+
+#include "MapView.h"
---
+++ MapView.h
@@ -0,0 +1,16 @@
+//
+// Created by gionimbus on 7/23/25.
+//
+
+#ifndef MAPVIEW_H
+#define MAPVIEW_H
+
+
+
+class MapView {
+
+};
+
+
+
+#endif //MAPVIEW_H
